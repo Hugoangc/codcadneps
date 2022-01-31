820319c573c53940d100643d4ba3ae8de78c68d4
Hugoangc <81435251+Hugoangc@users.noreply.github.com>
2022-01-31 14:05:34 -0300

    Create ordenacao_simples.cpp

---
+++ ordenacao_simples.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int v[1000000];
+    int n;
+    cin >> n;
+    for (int i = 0; i<n; i++){
+        cin>>v[i];
+    }
+    sort(v,v+n);
+    for (int i = 0; i < n; i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
