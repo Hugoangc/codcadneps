58ed664f4c7d72f5181b532ce22f047ea9fd5f29
Hugoangc <81435251+Hugoangc@users.noreply.github.com>
2022-01-31 13:47:23 -0300

    Create par_ou_impar.cpp

---
+++ par_ou_impar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int Bino, Cino;
+	cin >> Bino >> Cino;
+	
+	if ((Bino + Cino)%2 == 0){
+		cout << "Bino\n";
+	}else
+		cout << "Cino\n";
+return 0;
+}
